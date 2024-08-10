390cabf7570e9ff96dd80d49125e03458b5cc40f
Palvi Dhawan <palvidhawan@Palvis-MacBook-Air.local>
2024-08-10 19:54:06 +0530

    Added Socket to establish the connection

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
-import express from "express";
-import bodyParser from "body-parser";
+const express = require('express');
+const {createServer} = require("node:http");
+const {join} = require("node:path");
+const {Server} = require("socket.io");
 
 const app = express();
-app.use(bodyParser.json())
+const server = createServer(app);
+const io = new Server(server);
 
-app.all("/", (req, res) => {
-    res.send("I'm up");
+app.get('/', (req,res) => {
+    res.sendFile(join(__dirname, 'index.html'));
 });
 
-const PORT = 4000;
-app.listen(PORT, () => {
-    console.log(`server is running at ${PORT}`);
+io.on('connection', (socket) =>{
+    console.log("connection established");
+    socket.on('chat message', (msg) => { //listening to any custom event from client
+        console.log("recieved msg", msg);
+        socket.emit("chat message", msg); // broadcasting event to everyone from server
+    })
+
+    socket.on('disconnect', (msg) => {
+        console.log("user got disconnected!");
+    })
+})
+
+server.listen(3000, () => {
+    console.log(`Server running at Port: 3000`)
 })
